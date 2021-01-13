e7ec03500ac7eb8cdc8fdebb1b0e592e51ae07d2
Michal Miszczyszyn <michal@mmiszy.pl>
2021-01-13 02:14:33 +0100

    chore: Split test files

--- __tests__/tests.spec.ts
+++ __tests__/tests.spec.ts
@@ -1,8 +1,5 @@
-import { join } from 'path';
-
 import Fc from 'fast-check';
-import { complement, identity, is } from 'ramda';
-import tsd from 'tsd';
+import { complement, is } from 'ramda';
 
 import {
   validate,
@@ -41,24 +38,6 @@ const notThrows = <T extends readonly unknown[]>(predicate: (...args: T) => unkn
 const primitiveValidators = [number, string, date, boolean];
 
 describe('@typeofweb/schema', () => {
-  it('tsd', async () => {
-    const diagnostics = await tsd({
-      cwd: join(__dirname, '..'),
-      typingsFile: './dist/index.d.ts',
-      testFiles: ['./__tests__/*.test-d.ts'],
-    });
-
-    if (diagnostics.length > 0) {
-      const errorMessage = diagnostics.map((test) => {
-        return (
-          [test.fileName, test.line, test.column].filter(identity).join(':') +
-          ` - ${test.severity} - ${test.message}`
-        );
-      });
-      fail('\n' + errorMessage.join('\n') + '\n');
-    }
-  });
-
   describe('string', () => {
     it('should validate strings', () =>
       Fc.assert(Fc.property(Fc.string(), notThrows(validate(string())))));
---
+++ __tests__/types.spec.ts
@@ -0,0 +1,24 @@
+import { join } from 'path';
+
+import { identity } from 'ramda';
+import tsd from 'tsd';
+
+describe('@typeofweb/schema', () => {
+  it('tsd', async () => {
+    const diagnostics = await tsd({
+      cwd: join(__dirname, '..'),
+      typingsFile: './dist/index.d.ts',
+      testFiles: ['./__tests__/*.test-d.ts'],
+    });
+
+    if (diagnostics.length > 0) {
+      const errorMessage = diagnostics.map((test) => {
+        return (
+          [test.fileName, test.line, test.column].filter(identity).join(':') +
+          ` - ${test.severity} - ${test.message}`
+        );
+      });
+      fail('\n' + errorMessage.join('\n') + '\n');
+    }
+  });
+});
