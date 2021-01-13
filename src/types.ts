63b753c3b3787315016aa25c4ddc770dd153fc97
Michal Miszczyszyn <michal@mmiszy.pl>
2021-01-13 13:41:46 +0100

    feat(modifier): Added nonEmpty and minLength modifiers

--- src/types.ts
+++ src/types.ts
@@ -5,21 +5,21 @@ export type TypeOf<T extends AnySchema> =
   | If<T['__modifiers'], { readonly nullable: true }, null>
   | (T['__values'][number] extends never ? T['__type'] : T['__values'][number]);
 
-export interface Schema<
-  Type extends unknown,
-  Modifiers extends { readonly optional?: boolean; readonly nullable?: boolean },
-  Values
-> {
+type DefaultModifiers<L extends number = number> = {
+  readonly optional?: boolean;
+  readonly nullable?: boolean;
+  readonly minLength?: L;
+};
+
+export interface Schema<Type extends unknown, Modifiers extends DefaultModifiers, Values> {
   readonly __type: Type;
   readonly __validator: VALIDATORS;
   readonly __values: readonly Values[];
   readonly __modifiers: Modifiers;
 }
 
-export type AnySchema = Schema<
-  any,
-  { readonly optional?: boolean; readonly nullable?: boolean },
-  any
->;
+export type AnySchema = Schema<any, DefaultModifiers, any>;
+
+export type SomeSchema<T> = Schema<T, DefaultModifiers, any>;
 
 export type If<T, Obj, Y, N = never> = T extends Obj ? Y : N;
