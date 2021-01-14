01f695a9d5a889c500d325d469c0701616ded7b8
Michal Miszczyszyn <michal@mmiszy.pl>
2021-01-14 18:23:15 +0100

    feat: Add pipe and λ functions to easily compose validators

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export type { VALIDATORS, ValidatorToType } from './validators';
+export { minLength, nil, nonEmpty, nullable, optional } from './modifiers';
 export { oneOf, string, number, boolean, date, array, object, isSchema } from './validators';
 export { validate } from './validate';
-
-export type { AnySchema, Schema, SomeSchema, TypeOf } from './types';
-export { minLength, nil, nonEmpty, nullable, optional } from './modifiers';
 export { ValidationError } from './errors';
+export { λ, pipe } from './utils';
+export type { AnySchema, Schema, SomeSchema, TypeOf } from './types';
+export type { VALIDATORS, ValidatorToType } from './validators';
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import type { AnySchema } from './types';
+
 export const isDate = (d: unknown): d is Date =>
   Object.prototype.toString.call(d) === '[object Date]';
 
@@ -11,3 +13,25 @@ export const fromEntries = (entries: readonly (readonly [string | number, any])[
 
   return obj;
 };
+
+type Modifier<S1, S2> = (arg: S1) => S2;
+type SchemaArg<S> = (() => S) | S;
+// prettier-ignore
+export function pipe<S1>(schema: SchemaArg<S1>): S1;
+// prettier-ignore
+export function pipe<S1, S2>(schema: SchemaArg<S1>, mod1: Modifier<S1, S2>): S2;
+// prettier-ignore
+export function pipe<S1, S2, S3>(schema: SchemaArg<S1>, mod1: Modifier<S1, S2>, mod2: Modifier<S2, S3>): S3;
+// prettier-ignore
+export function pipe<S1, S2, S3, S4>(schema: SchemaArg<S1>, mod1: Modifier<S1, S2>, mod2: Modifier<S2, S3>, mod3: Modifier<S3, S4>): S4;
+// prettier-ignore
+export function pipe<S1, S2, S3, S4, S5>(schema: SchemaArg<S1>, mod1: Modifier<S1, S2>, mod2: Modifier<S2, S3>, mod3: Modifier<S3, S4>, mod4: Modifier<S4, S5>): S5;
+// prettier-ignore
+export function pipe<S1, S2, S3, S4, S5, S6>(schema: SchemaArg<S1>, mod1: Modifier<S1, S2>, mod2: Modifier<S2, S3>, mod3: Modifier<S3, S4>, mod4: Modifier<S4, S5>, mod5: Modifier<S5, S6>): S6;
+export function pipe<T extends AnySchema>(
+  schema: SchemaArg<T>,
+  ...rest: readonly ((...args: readonly any[]) => any)[]
+) {
+  return rest.reduce((acc, mod) => mod(acc), typeof schema === 'function' ? schema() : schema);
+}
+export const λ = pipe;
